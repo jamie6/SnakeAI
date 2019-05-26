4a01eb2d82dd73fdb4bc64b59d8fbb32defc671a
jamie6 <42789130+jamie6@users.noreply.github.com>
2019-05-26 12:24:43 -0700

    Update SnakeAI.java

--- SnakeAI.java
+++ SnakeAI.java
@@ -12,24 +12,52 @@ public class SnakeAI
 
     public static void main(String[] args)
     {
-        String[] options = {"Train Snake A.I.", "Test Snake A.I.", "Play Snake Game"};
-        String choice = (String)JOptionPane.showInputDialog(
-                            null, "Choose an option: ", "Snake A.I. start menu",
-                            JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+        if ( args.length == 0 )
+        {
+            String[] options = {"Train Snake A.I.", "Test Snake A.I.", "Play Snake Game"};
+            String choice = (String)JOptionPane.showInputDialog(
+                                null, "Choose an option: ", "Snake A.I. start menu",
+                                JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 
-        if ( options[0].equals(choice) )
-        {
-            window = new TrainSnakeWindow();
+            if ( options[0].equals(choice) )
+            {
+                window = new TrainSnakeWindow();
+            }
+            else if (options[1].equals(choice))
+            {
+                window = new TestSnakeWindow();
+            }
+            else if ( options[2].equals(choice))
+            {
+                window = new PlaySnakeWindow();
+            }
+            if ( window != null ) mainLoop();
         }
-        else if (options[1].equals(choice))
+        else if ( args.length == 1 )
         {
-            window = new TestSnakeWindow();
+            if ( args[0].equals("1") )
+            {
+                window = new TrainSnakeWindow();
+            }
+            else if (args[0].equals("2"))
+            {
+                window = new TestSnakeWindow();
+            }
+            else if ( args[0].equals("3"))
+            {
+                window = new PlaySnakeWindow();
+            }
+            if ( window != null ) mainLoop();
         }
-        else if ( options[2].equals(choice))
+        else if ( args.length == 3 )
         {
-            window = new PlaySnakeWindow();
+            int population = Integer.parseInt(args[0]);
+            int hiddenNodes = Integer.parseInt(args[1]);
+            int hiddenLayers = Integer.parseInt(args[2]);
+            
+            window = new TrainSnakeWindow(population, hiddenNodes, hiddenLayers);
+            mainLoop();
         }
-        if ( window != null ) mainLoop();
     }
     
     public static void mainLoop()
